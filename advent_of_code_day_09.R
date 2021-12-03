c712070de9937a3dd9f24c0457f513d301b9ce6f
Martin Gal <15073494+martin-gal@users.noreply.github.com>
2021-12-03 01:02:51 +0100

    Tag 9
    - int_code_computer weiterentwickelt
    - Fehler in Verarbeitung

--- advent_of_code_day_09.R
+++ advent_of_code_day_09.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 
-input <- read_csv("aoc_data/day_9_test_1.csv.csv",
+input <- read_csv("aoc_data/day_9.csv",
                   col_names = FALSE)
 
 # Teil 1
@@ -12,3 +12,22 @@ vec <- input %>%
   as.numeric()
 
 source("intcode_computer_v2.R")
+
+debug <- TRUE
+
+
+output <- list(prog = vec, index = 1, input = 1, base = 0)
+
+# Schleife starten
+while(TRUE) {
+  output <- intcode_comp(
+    output[["prog"]],
+    output[["index"]],
+    output[["input"]],
+    output[["base"]]
+    )
+
+  output[["prog"]][is.na(output[["prog"]])] <- 0
+
+  if (output[["breaker"]]) { break }
+}
--- intcode_computer_v2.R
+++ intcode_computer_v2.R
@@ -1,4 +1,4 @@
-intcode_comp <- function(prog, idx, input = NULL) {
+intcode_comp <- function(prog, idx, input = NULL, base) {
   # ABCDE
   # DE - OpCode
   # C  - Parameter 1
@@ -16,104 +16,144 @@ intcode_comp <- function(prog, idx, input = NULL) {
     parameter_1 <- prog[prog[idx + 1] + 1]
   } else if (para_c == 1){
     parameter_1 <- prog[idx + 1]
+  } else if (para_c == 2) {
+    parameter_1 <- prog[prog[idx + 1] + 1 + base]
   }
 
   if (para_b == 0) {
     parameter_2 <- prog[prog[idx + 2] + 1]
   } else if (para_b == 1){
     parameter_2 <- prog[idx + 2]
+  } else if (para_b == 2) {
+    parameter_2 <- prog[prog[idx + 2] + 1 + base]
+  }
+
+  if (para_a == 0) {
+    target <- prog[idx + 3] + 1
+  } else if (para_a == 1) {
+    target <- idx + 3
+  } else if (para_a == 2) {
+    target <- prog[idx + 3] + 1 + base
   }
 
   if (debug) print(paste0("OpCode: ", opcode))
 
-# Addition (drei Parameter)
+
   if (opcode == 1) {
-    if (para_a == 0) {
-      prog[prog[idx + 3] + 1] <- parameter_1 + parameter_2
-    } else if (para_a == 1){
-      prog[idx + 3] <- parameter_1 + parameter_2
-    }
+    # Addition (drei Parameter)
+    prog[target] <- parameter_1 + parameter_2
 
-    return(list(prog = prog, index = idx + 4, breaker = FALSE))
+    return(list(prog = prog,
+                index = idx + 4,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
   } else if (opcode == 2) {
-# Multiplikation (drei Parameter)
-    if (para_a == 0) {
-      prog[prog[idx + 3] + 1] <- parameter_1 * parameter_2
-    } else if (para_a == 1){
-      prog[idx + 3] <- parameter_1 * parameter_2
-    }
+    # Multiplikation (drei Parameter)
+    prog[target] <- parameter_1 * parameter_2
 
-    return(list(prog = prog, index = idx + 4, breaker = FALSE))
+    return(list(prog = prog,
+                index = idx + 4,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
   } else if (opcode == 3) {
 # Eingabe in Zelle schreiben (ein Parameter)
     prog[prog[idx + 1] + 1] <- input
-    return(list(prog = prog, index = idx + 2, breaker = FALSE))
+
+    return(list(prog = prog,
+                index = idx + 2,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
   } else if (opcode == 4) {
 # Wert ausgeben (ein Parameter)
     print(paste0("Ausgabe: ", parameter_1))
 
-    return(list(prog = prog, index = idx + 2, breaker = FALSE))
+    return(list(prog = prog,
+                index = idx + 2,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
   } else if (opcode == 5) {
 # Jump-if-True (zwei Parameter)
     if (parameter_1 != 0) {
-      return(list(prog = prog, index = parameter_2 + 1, breaker = FALSE))
+      return(list(prog = prog,
+                  index = parameter_2 + 1,
+                  input = input,
+                  breaker = FALSE,
+                  base = base))
+
     } else {
-      return(list(prog = prog, index = idx + 3, breaker = FALSE))
+      return(list(prog = prog,
+                  index = idx + 3,
+                  input = input,
+                  breaker = FALSE,
+                  base = base))
     }
   } else if (opcode == 6) {
 # Jump-if-False (zwei Parameter)
     if (parameter_1 == 0) {
-      return(list(prog = prog, index = parameter_2 + 1, breaker = FALSE))
+      return(list(prog = prog, index = parameter_2 + 1, input = input, breaker = FALSE, base = base))
     } else {
-      return(list(prog = prog, index = idx + 3, breaker = FALSE))
+      return(list(prog = prog, index = idx + 3, input = input, breaker = FALSE, base = base))
     }
   } else if (opcode == 7) {
 # Kleiner-als
     # print(paste0("Parameter 1: ", parameter_1))
     # print(paste0("Parameter 2: ", parameter_2))
     # print(paste0("Kleiner: ", parameter_1 < parameter_2))
+    if (parameter_1 < parameter_2) {
+        prog[target] <- 1
+        } else {
+          prog[target] <- 0
+          }
 
-    if (para_a == 0) {
-      if (parameter_1 < parameter_2) {
-        prog[prog[idx + 3] + 1] <- 1
-      } else {
-        prog[prog[idx + 3] + 1] <- 0
-      }
-    } else if (para_a == 1){
-      if (parameter_1 < parameter_2) {
-        prog[idx + 3] <- 1
-      } else {
-        prog[idx + 3] <- 0
-      }
-    }
-
-    return(list(prog = prog, index = idx + 4, breaker = FALSE))
+    return(list(prog = prog, index = idx + 4, input = input, breaker = FALSE, base = base))
   } else if (opcode == 8) {
 # Gleichheit
     # print(paste0("Parameter 1 ", parameter_1))
     # print(paste0("Parameter 2 ", parameter_2))
     # print(paste0("Gleichheit: ", parameter_1 == parameter_2))
 
-    if (para_a == 0) {
-      if (parameter_1 == parameter_2) {
-        prog[prog[idx + 3] + 1] <- 1
-      } else {
-        prog[prog[idx + 3] + 1] <- 0
-      }
-    } else if (para_a == 1){
-      if (parameter_1 == parameter_2) {
-        prog[idx + 3] <- 1
-      } else {
-        prog[idx + 3] <- 0
-      }
+    if (parameter_1 == parameter_2) {
+      prog[target] <- 1
+    } else {
+      prog[target] <- 0
     }
 
-    return(list(prog = prog, index = idx + 4, breaker = FALSE))
+    return(list(prog = prog,
+                index = idx + 4,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
+  } else if (opcode == 9){
+    base <- base + parameter_1
+
+    return(list(prog = prog,
+                index = idx + 2,
+                input = input,
+                breaker = FALSE,
+                base = base))
+
   } else if (opcode == 99) {
 # Programm beenden
+
     message("Programm erfolgreich beendet.")
-    return(list(prog = prog, index = idx, breaker = TRUE))
+
+    return(list(prog = prog,
+                index = idx,
+                input = input,
+                breaker = TRUE,
+                base = base))
+
   } else {
+
     stop("Fehler!")
   }
 }
